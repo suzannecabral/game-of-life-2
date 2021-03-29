296584d99b2b7b40e361e08e0f9117edf86225b6
Suzanne Cabral <suzannecabral@live.com>
2021-03-29 10:47:50 -0700

    cleaned up gridSquare

--- src/grid/GridSquare.jsx
+++ src/grid/GridSquare.jsx
@@ -6,15 +6,13 @@ import { randomRange } from '../utils';
 
 function GridSquare(props){
     let {row, square} = props;
-    let squareValue = square;
-    // let coords = [row, square]
     let clickToggle = (square) => {
-        // square===0 ? square = 1 : square = 0;
-        if(square==0){
+        if(square===0){
             square = 1;
         }else{
             square = 0;
         }
+        console.log('toggled');
     }
 
     const handleClick = (event) => {
@@ -24,16 +22,11 @@ function GridSquare(props){
         console.log('square: ', square);
     }
 
-    // useEffect(()=>{
-    //     setStyles(square);
-    // },[]);
-
     return(
         <button 
             className={square===0 ? 'grid-square false' : 'grid-square true'} 
             onClick={(event)=>{handleClick(event)}} 
         >
-            {squareValue}
         </button>
     )
 }
